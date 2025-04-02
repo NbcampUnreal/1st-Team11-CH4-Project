3e3d37dcdaec81bf26990fe397e1e43ab38b50b0
meo-young <meoyoung000@naver.com>
2025-04-02 21:45:13 +0900

    [Update] 보스 특수 공격 조건 체크 구현

--- Source/ElvenRing/Boss/BossPattern/BossSpecialPatternComponent.cpp
+++ Source/ElvenRing/Boss/BossPattern/BossSpecialPatternComponent.cpp
@@ -5,15 +5,10 @@ UBossSpecialPatternComponent::UBossSpecialPatternComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 	PatternCount = 0;
+	PossibleSpecialPatternIndex = 0;
 }
 
-void UBossSpecialPatternComponent::TickComponent(float DeltaTime, enum ELevelTick TickType,
-                                                 FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	ExecuteAttackPattern();
-}
 
 void UBossSpecialPatternComponent::ExecuteAttackPattern()
 {
@@ -24,36 +19,24 @@ void UBossSpecialPatternComponent::ExecuteAttackPattern()
 	}
 
 	// 실행 가능한 특수공격 패턴의 인덱스를 가져와 실행
-	int8 PossibleSpeicalPatternIndex = GetPossibleSpecialPattern();
-	if (Patterns.IsValidIndex(PossibleSpeicalPatternIndex))
+	if (Patterns.IsValidIndex(PossibleSpecialPatternIndex))
 	{
-		Patterns[PossibleSpeicalPatternIndex].Execute();
+		Patterns[PossibleSpecialPatternIndex].Execute();
 	}
 }
 
 
 
-void UBossSpecialPatternComponent::AddAttackPattern(const FSpecialAttackConditionDelegate& InCondition,
-                                                    const FAttackExecuteDelegate& InExecute, const FString& InName)
-{
-	PatternConditions[PatternCount] = InCondition;
-	Patterns[PatternCount] = InExecute;
-	PatternName[PatternCount] = InName;
-	++PatternCount;
-}
-
-
-
-int8 UBossSpecialPatternComponent::GetPossibleSpecialPattern()
+bool UBossSpecialPatternComponent::IsSpecialPatternAvailable()
 {
 	for (int8 i = 0; i < PatternCount; ++i)
 	{
 		if (PatternConditions[i].IsBound() && PatternConditions[i].Execute())
 		{
-			LOG(TEXT("Pattern %d is Available"), i);
-			return i;
+			PossibleSpecialPatternIndex = i;
+			return true;
 		}
 	}
-	
-	return -1;
+
+	return false;
 }
