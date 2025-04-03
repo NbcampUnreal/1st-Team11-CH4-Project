474a7cf70413f91729bd1e19ad929335e236d778
handakim25 <handakim25@gmail.com>
2025-04-03 19:41:45 +0900

    Add: 레벨 이동 구현

--- Source/ElvenRing/Core/ElvenRingGameMode.cpp
+++ Source/ElvenRing/Core/ElvenRingGameMode.cpp
@@ -2,3 +2,20 @@
 
 
 #include "ElvenRingGameMode.h"
+
+AElvenRingGameMode::AElvenRingGameMode()
+{
+	bActorSeamlessTraveled = true;
+}
+
+void AElvenRingGameMode::HandleLevelTransition(APlayerController* PlayerController, const FString& LevelName)
+{
+	if (GetNetMode() == NM_Standalone)
+	{
+		PlayerController->ClientTravel(LevelName, TRAVEL_Absolute);
+	}
+	else
+	{
+		GetWorld()->ServerTravel(LevelName);
+	}
+}
--- Source/ElvenRing/Core/ElvenringGameInstance.cpp
+++ Source/ElvenRing/Core/ElvenringGameInstance.cpp
@@ -2,3 +2,47 @@
 
 
 #include "ElvenringGameInstance.h"
+
+#include "Blueprint/UserWidget.h"
+
+void UElvenringGameInstance::Init()
+{
+	Super::Init();
+
+	FCoreUObjectDelegates::PreLoadMap.AddUObject(this, &UElvenringGameInstance::OnPreLoadMap);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &UElvenringGameInstance::OnPostLoadMap);
+}
+
+void UElvenringGameInstance::ShowLoadingScreen()
+{
+	UE_LOG(LogTemp, Display, TEXT("ShowLoadingScreen"));
+	if (!LoadingScreenWidget && LoadingScreenClass)
+	{
+		LoadingScreenWidget = CreateWidget<UUserWidget>(this, LoadingScreenClass);
+		if (LoadingScreenWidget)
+		{
+			LoadingScreenWidget->AddToViewport(9999);
+		}
+	}
+}
+
+void UElvenringGameInstance::HideLoadingScreen()
+{
+	if (LoadingScreenWidget)
+	{
+		LoadingScreenWidget->RemoveFromParent();
+		LoadingScreenWidget = nullptr;
+	}
+}
+
+void UElvenringGameInstance::OnPreLoadMap(const FString& String)
+{
+	UE_LOG(LogTemp, Display, TEXT("OnPreLoadMap: %s"), *String);
+	ShowLoadingScreen();
+}
+
+void UElvenringGameInstance::OnPostLoadMap(UWorld* World)
+{
+	UE_LOG(LogTemp, Display, TEXT("OnPostLoadMap: %s"), *World->GetName());
+	HideLoadingScreen();
+}
