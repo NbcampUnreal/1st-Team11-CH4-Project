9d19440b8d888e75e1249e6d4cfb6716dae44525
handakim25 <handakim25@gmail.com>
2025-04-07 12:39:10 +0900

    Add: Game Flag Event Listener 추가

---
+++ Source/ElvenRing/Gimmick/EventListenerComponent.h
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EventListenerComponent.generated.h"
+
+UENUM()
+enum class EConditionType : UINT8
+{
+	All,
+	Any,
+};
+
+USTRUCT(Blueprintable)
+struct FCondition
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	class UGameFlag* TargetGameFlag;
+	UPROPERTY(EditDefaultsOnly)
+	bool bExpectedValue;
+};
+
+USTRUCT(Blueprintable)
+struct FRequirement
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FCondition> Conditions;
+	UPROPERTY(EditDefaultsOnly)
+	EConditionType ConditionType;
+
+	bool Evaluate(const class UEventManager* EventEvaluator) const;
+	void UpdateCondition(const class UEventManager* EventEvaluator);
+	bool IsMet = false;
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ELVENRING_API UEventListenerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UEventListenerComponent();
+	
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnEventTriggered);
+	UPROPERTY(BlueprintAssignable)
+	FOnEventTriggered OnEventTriggered;
+	
+protected:
+	UFUNCTION()
+	void OnFlagChanged(bool bNewValue, bool bOldValue);
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	FRequirement Requirement;
+};
