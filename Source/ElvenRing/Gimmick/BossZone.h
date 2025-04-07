4468170e5de06b9c1ffbb4c3ebfd7b468e7e3656
Big_Baek <euihyun1188@gmail.com>
2025-04-07 21:26:12 +0900

    .

---
+++ Source/ElvenRing/Gimmick/BossZone.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "BossZone.generated.h"
+
+UCLASS()
+class ELVENRING_API ABossZone : public ATriggerBox
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABossZone();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bCanSpawnBoss = false;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
---
+++ Source/ElvenRing/Interaction/FlagTrigger.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "GameFramework/Actor.h"
+#include "FlagTrigger.generated.h"
+
+UCLASS()
+class ELVENRING_API AFlagTrigger : public AActor, public IInteractable
+{
+public:
+	virtual FString GetInteractText() override;
+	virtual void Interact(APlayerController* Player) override;
+	virtual bool CanInteract() override;
+
+private:
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFlagTrigger();
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnEventTriggered);
+	UPROPERTY(BlueprintAssignable)
+	FOnEventTriggered OnEventTriggered;
+	
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	class UGameFlag* GameFlagAsset;
+	UPROPERTY(EditAnywhere)
+	bool TargetCondition;
+};
