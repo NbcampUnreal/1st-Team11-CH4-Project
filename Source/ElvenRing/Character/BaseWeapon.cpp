e839c0d14e4963ecb6ec9c68c5d68a741d6d42b8
GS8LIFE <gs.8life@gmail.com>
2025-04-07 16:15:35 +0900

    조선욱 : 공격 적용

---
+++ Source/ElvenRing/Character/BaseWeapon.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseWeapon.h"
+#include "Components/BoxComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Controller.h"
+// Sets default values
+ABaseWeapon::ABaseWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	RootComponent = WeaponMesh;
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	CollisionBox->SetupAttachment(RootComponent);
+	CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CollisionBox->SetCollisionObjectType(ECC_WorldDynamic);
+	CollisionBox->SetCollisionResponseToAllChannels(ECR_Overlap);
+	CollisionBox->SetGenerateOverlapEvents(true);
+}
+
+// Called when the game starts or when spawned
+void ABaseWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ABaseWeapon::OnOverlapBegin);
+}
+
+// Called every frame
+void ABaseWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseWeapon::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{if (OtherActor && OtherActor != this)
+{
+	float Damage = 20.0f;
+
+	AController* InstigatorController = GetInstigatorController();
+
+	UGameplayStatics::ApplyDamage(OtherActor, Damage, InstigatorController, this, UDamageType::StaticClass());
+}
+}
+
