6c69604920088a1a9c6ba88f253e7a0dd61ebb4c
meo-young <meoyoung000@naver.com>
2025-04-12 18:02:53 +0900

    [Update] HangedMan 패턴 2개 추가

--- Source/ElvenRing/Boss/HangedMan/BossHangedMan.cpp
+++ Source/ElvenRing/Boss/HangedMan/BossHangedMan.cpp
@@ -1,9 +1,33 @@
 #include "BossHangedMan.h"
-
 #include "ElvenRing/ElvenRing.h"
+#include "EngineUtils.h"
+#include "ElvenRing/Boss/BossPattern/BossNormalPatternComponent.h"
 
 ABossHangedMan::ABossHangedMan()
 {
+	HealthPhase = EHealthPhase::None;
+	LightningInterval = 0.5f;
+	MaxLightningCount = 6;
+	CurrentLightningCount = 0;
+	LightningStartDelay = 1.5f;
+	PlayerCount = 0;
+}
+
+void ABossHangedMan::BeginPlay()
+{
+	//NormalPattern->AddAttackPattern(this, &ABossHangedMan::LightningAttack, FString("LightningAttack"));
+	NormalPattern->AddAttackPattern(this, &ABossHangedMan::ElectricJavelinAttack, FString("ElectricJavelinAttack"));
+	//NormalPattern->AddAttackPattern(this, &ABossHangedMan::ThunderBoldAttack, FString("ThunderBoldAttack"));
+	//NormalPattern->AddAttackPattern(this, &ABossHangedMan::ElectronicAttack, FString("ElectronicAttack"));
+
+	Super::BeginPlay();
+	SetBossBattleMode();
+}
+
+void ABossHangedMan::OnDeath()
+{
+	GetWorldTimerManager().ClearTimer(LightningTimer);
+	Super::OnDeath();
 }
 
 void ABossHangedMan::ServerOnSpawnSequenceEnded_Implementation()
@@ -14,11 +38,6 @@ void ABossHangedMan::ServerOnSpawnSequenceEnded_Implementation()
 void ABossHangedMan::ServerOnPhaseSequenceEnded_Implementation()
 {
 	Super::ServerOnPhaseSequenceEnded_Implementation();
-}
-
-void ABossHangedMan::BeginPlay()
-{
-	Super::BeginPlay();
 }
 
 float ABossHangedMan::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator,
@@ -33,20 +52,78 @@ void ABossHangedMan::CheckHealthPhase()
 {
 	if (CurHealth <= MaxHealth/4*3 && HealthPhase == EHealthPhase::None)
 	{
-		LOG(TEXT("3/4"));
 		HealthPhase = EHealthPhase::First;
 		PlayAnimation(GroggyAnim);
 	}
 	else if (CurHealth <= MaxHealth/2 && HealthPhase == EHealthPhase::First)
 	{
-		LOG(TEXT("1/2"));
 		HealthPhase = EHealthPhase::Second;
 		PlayAnimation(GroggyAnim);
 	}
 	else if (CurHealth <= MaxHealth/4 && HealthPhase == EHealthPhase::Second)
 	{
-		LOG(TEXT("1/4"));
 		HealthPhase = EHealthPhase::Third;
 		PlayAnimation(GroggyAnim);
 	}
+}
+
+void ABossHangedMan::LightningAttack()
+{
+	PlayAnimation(LightningAnim);
+
+	if (HasAuthority())
+	{
+		GetWorldTimerManager().SetTimer(LightningTimer, this, &ABossHangedMan::GetAllPlayersLocation, LightningInterval, true, LightningStartDelay);
+	}
+}
+
+void ABossHangedMan::ElectricJavelinAttack()
+{
+	PlayAnimation(ElectricJavelinAnim);
+}
+
+void ABossHangedMan::ThunderBoldAttack()
+{
+	PlayAnimation(ThunderBoldAnim);
+}
+
+void ABossHangedMan::ElectronicAttack()
+{
+	PlayAnimation(ElectronicAnim);
+}
+
+void ABossHangedMan::MulticastLightning_Implementation()
+{
+	for (int8 i=0; i<PlayerCount; ++i)
+	{
+		FVector SpawnLocation = FVector( PlayersLocation[i].X, PlayersLocation[i].Y, -2100.f );
+		GetWorld()->SpawnActor<AActor>(LightningNiagara, SpawnLocation, FRotator::ZeroRotator);
+	}
+
+	++CurrentLightningCount;
+	
+	if (CurrentLightningCount >= MaxLightningCount)
+	{
+		CurrentLightningCount = 0;
+		GetWorldTimerManager().ClearTimer(LightningTimer);
+	}
+}
+
+void ABossHangedMan::GetAllPlayersLocation()
+{
+	PlayersLocation.Empty();
+	PlayerCount = 0;
+	
+	for (AElvenRingCharacter* Player : TActorRange<AElvenRingCharacter>(GetWorld()))
+	{
+		if (IsValid(Player))
+		{
+			PlayersLocation.Add(Player->GetActorLocation());
+			++PlayerCount;
+		}
+	}
+
+	FTimerHandle MulticastLightningTimer;
+	GetWorldTimerManager().SetTimer(MulticastLightningTimer, this,
+		&ABossHangedMan::MulticastLightning_Implementation, LightningInterval, false, true);
 }
--- Source/ElvenRing/Boss/HangedMan/BossHangedMan.h
+++ Source/ElvenRing/Boss/HangedMan/BossHangedMan.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EngineUtils.h"
 #include "ElvenRing/Boss/Boss.h"
+#include "ElvenRing/Character/ElvenRingCharacter.h"
 #include "BossHangedMan.generated.h"
+
+class ATargetPoint;
 
 UENUM()
 enum class EHealthPhase : uint8
@@ -26,19 +30,64 @@ public:
 	
 protected:
 	virtual void BeginPlay() override;
+	virtual void OnDeath() override;
 	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
 
 private:
 	/** 보스의 현재 체력에 따라 그로기 애니메이션 출력 판정 */
 	void CheckHealthPhase();
+	
+	UFUNCTION(BlueprintCallable)
+	void LightningAttack();
+
+	void ElectricJavelinAttack();
+
+	void ThunderBoldAttack();
+
+	void ElectronicAttack();
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastLightning();
+
+	void GetAllPlayersLocation();
 
 protected:
 	
 
 private:
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Pattern")
+	TSubclassOf<AActor> LightningNiagara;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Pattern")
+	float LightningStartDelay;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Pattern")
+	float LightningInterval;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Pattern")
+	int8 MaxLightningCount;
+
 	UPROPERTY(EditDefaultsOnly, Category = "Boss|Anim")
-	TObjectPtr<class UAnimMontage> GroggyAnim;
+	TObjectPtr<UAnimMontage> LightningAnim;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Anim")
+	TObjectPtr<UAnimMontage> GroggyAnim;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Anim")
+	TObjectPtr<UAnimMontage> ElectricJavelinAnim;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Anim")
+	TObjectPtr<UAnimMontage> ThunderBoldAnim;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Anim")
+	TObjectPtr<UAnimMontage> ElectronicAnim;
 	
 	EHealthPhase HealthPhase;
 	
+	FTimerHandle LightningTimer;
+
+	int8 CurrentLightningCount;
+
+	TArray<FVector> PlayersLocation;
+	int8 PlayerCount;
 };
