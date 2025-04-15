5a2cc146dfc8aabc9bdc52e8eb69c574389ece38
handakim25 <handakim25@gmail.com>
2025-04-16 02:22:34 +0900

    Add: Interaction Message Widget 추가

---
+++ Source/ElvenRing/UI/InteractionMessageWidget.cpp
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractionMessageWidget.h"
+
+#include "Components/TextBlock.h"
+
+void UInteractionMessageWidget::ShowMessageText_Implementation(const FString& Message)
+{
+	if (GetVisibility() == ESlateVisibility::Collapsed)
+		SetVisibility(ESlateVisibility::Visible);
+	
+	TextMessage->SetText(FText::FromString(Message));
+	PlayFadeInAnimation();
+}
+
+void UInteractionMessageWidget::HideMessageText()
+{
+	PlayFadeOutAnimation();
+}
+
+void UInteractionMessageWidget::OnFadeAnimationEnd()
+{
+	if (bIsForwarded)
+	{
+		SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		SetVisibility(ESlateVisibility::Collapsed);
+	}
+}
+
+void UInteractionMessageWidget::PlayFadeInAnimation()
+{
+	if (FadeAnim)
+	{
+		bIsForwarded = true;
+		OnFadeFinishedDelegate.Unbind();
+		OnFadeFinishedDelegate.BindDynamic(this, &UInteractionMessageWidget::OnFadeAnimationEnd);
+		
+		UnbindAllFromAnimationFinished(FadeAnim);
+		BindToAnimationFinished(FadeAnim, OnFadeFinishedDelegate);
+		
+		PlayAnimationForward(FadeAnim);
+	}
+}
+
+void UInteractionMessageWidget::PlayFadeOutAnimation()
+{
+	if (FadeAnim)
+	{
+		bIsForwarded = false;
+		OnFadeFinishedDelegate.Unbind();
+		OnFadeFinishedDelegate.BindDynamic(this, &UInteractionMessageWidget::OnFadeAnimationEnd);
+		
+		UnbindAllFromAnimationFinished(FadeAnim);
+		BindToAnimationFinished(FadeAnim, OnFadeFinishedDelegate);
+		
+		PlayAnimationReverse(FadeAnim);
+	}
+}
+
+void UInteractionMessageWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UInteractionMessageWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+}
---
+++ Source/ElvenRing/UI/InteractionMessageWidget.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MessageWidgetBase.h"
+#include "InteractionMessageWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELVENRING_API UInteractionMessageWidget : public UMessageWidgetBase
+{
+	GENERATED_BODY()
+
+	virtual void ShowMessageText_Implementation(const FString& Message) override;
+	void HideMessageText();
+
+protected:
+	// Forward : Fade In, Reverse : Fade Out
+	bool bIsForwarded;
+	UFUNCTION()
+	void OnFadeAnimationEnd();
+	void PlayFadeInAnimation();
+	void PlayFadeOutAnimation();
+	
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+};
